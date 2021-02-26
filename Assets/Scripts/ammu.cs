3299af0e118f1d47ef6c48c6dd99804b9f4298a4
Moi-123 <jan.puonti05@gmail.com>
2021-02-26 17:55:29 +0200

    koronatallennus

---
+++ Assets/Scripts/ammu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammu : MonoBehaviour
+{
+    Camera FPSCamera;
+
+    public float throwForce = 10f;
+    public GameObject throwObjPrefab;
+    private GameObject ammus = null;
+
+    public LineRenderer Ir;
+
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+
+        if (gameObject.CompareTag("laserase"))
+        {
+
+            Ir.enabled = false;
+
+        }
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && gameObject.CompareTag("ase"))
+        {
+            Throw();
+
+        }
+        if (Input.GetButtonDown("Fire2") && gameObject.CompareTag("laserase"))
+        {
+            //Laser();
+            StartCoroutine(Laser());
+        }
+    }
+
+    public void Throw()
+    {
+
+        ammus = Instantiate(throwObjPrefab, transform.position, Quaternion.identity);
+        ammus.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * throwForce, ForceMode.Impulse);
+    }
+
+    public IEnumerator Laser()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            Ir.SetPosition(0, transform.position);
+            Ir.SetPosition(1, hit.point);
+
+            if (hit.collider.gameObject.name == "Zombie1")
+            {
+
+                anim.SetBool("ammuttu", true);
+               }
+
+            Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(FPSCamera.transform.forward * 100, ForceMode.Impulse);
+            }
+
+
+        }
+
+        Ir.enabled = true;
+        yield return new WaitForSecondsRealtime(2.6f);
+        Ir.enabled = false;
+
+
+    }
+}
